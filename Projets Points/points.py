1c53b6464b25a570ad5b70acc65119faa86398cc
Theo5607 <53973847+Theo5607@users.noreply.github.com>
2022-03-23 09:39:49 +0100

    Update points.py

--- Projets Points/points.py
+++ Projets Points/points.py
@@ -1,6 +1,7 @@
 from math import *
 from random import *
 from copy import *
+import timeit
 
 liste_points = []
 x_max=int(input("Donnez une abcisse maximum: "))
@@ -23,7 +24,10 @@ def distance_minimal_force_brute(pts):
                 min=dist
     return min
 
+debut = timeit.default_timer()
 print(distance_minimal_force_brute(liste_points))
+fin = timeit.default_timer()
+print(fin-debut)
 #il y a n**2 appels de la fonction distance
 
 def trier(L, i):
@@ -56,9 +60,30 @@ def distance_minimal_dpr(px,py):
     long=len(px)
     px1=px[:long//2]
     px2=px[long//2:]
-    if len(px1)<=1 or len(px2)<=1:
-        return sqrt((px2[0][0]-px1[0][0])**2+(px2[0][1]-px1[0][1])**2)
     m=(px1[-1][0]+px2[0][0])//2
+    if len(px1)<=1 or len(px2)<=1:
+        return (sqrt((px2[0][0]-px1[0][0])**2+(px2[0][1]-px1[0][1])**2),m)
     return min(distance_minimal_dpr(px1, [el for el in py if el[1]<=m]), distance_minimal_dpr(px2, [el for el in py if el[1]>m]))
 
-print(distance_minimal_dpr(px, py))
+def dist_minimal(px,py):
+    val=distance_minimal_dpr(px,py)
+    delta,m=val[0],val[1]
+    l_pts=[]
+    for el in py:
+        if el[0]>=m-delta and el[0]<=m+delta:
+            l_pts.append(el)
+    min=sqrt((py[-1][0]-py[0][0])**2+(py[-1][1]-py[0][1])**2)
+    for i in range(len(py)):
+        for j in range(i+7):
+            if i+j>=len(py):
+                break
+            if i!=i+j:
+                dist=sqrt((py[i+j][0]-py[i][0])**2+(py[i+j][1]-py[i][1])**2)
+                if dist<min:
+                    min=dist
+    return min
+
+debut=timeit.default_timer()
+print(dist_minimal(px,py))
+fin=timeit.default_timer()
+print(fin-debut)
